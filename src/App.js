48b47e3484627919badd0f5d78a684de7f14beec
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-12 23:48:07 +0300

    Added the NavBar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,35 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-        <h1>e-Commerce App</h1>
-      </div>
+    <div classNameName="App">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-style">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            e-Commerce App
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
