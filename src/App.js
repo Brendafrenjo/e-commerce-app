0b3552d64117f9be847b6dcb4e5229900fea27d3
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-13 23:50:43 +0300

    imported NavBar and MianContent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./NavBar";
-import MainContent from "./MainContent"
+import MainContent from "./MainContent";
 
 function App() {
   return (
