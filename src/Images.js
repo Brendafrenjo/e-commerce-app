c1de9531f4c21fc996526b579d84967fd810a539
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-14 01:03:58 +0300

    Added Logo

---
+++ src/Images.js
@@ -0,0 +1,10 @@
+import React from "react";
+import img1 from ",/Images/Frenjo's logo";
+
+export default function Images() {
+  return (
+    <div>
+      <img src={img1} alt="Logo"></img>
+    </div>
+  );
+}
