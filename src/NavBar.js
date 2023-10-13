54bfd3778cfc2ab2333a3b57fa55f4cea07e7214
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-14 00:46:13 +0300

    Updated NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,39 +1,62 @@
 import React from "react";
-import "./NavBar.css"
+import "./NavBar.css";
 
 export default function NavBar() {
-    return (
-      <div className="NavBar">
-        <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-style">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              e-Commerce App
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Home
-                  </a>
-                </li>
-              </ul>
+  return (
+    <div className="NavBar">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-style">
+        <div className="container-fluid">
+          <a className="navbar-brand fw-bold fs-4" href="/">
+            BREE's ESSENTIALS
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Products
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <div className="buttons">
+              <a href="/" className="btn btn-outline-dark me-2">
+                <i className="fa-solid fa-right-to-bracket me-2 icon"></i>Login
+              </a>
+              <a href="/" className="btn btn-outline-dark me-2">
+                <i class="fa-solid fa-user-plus me-2 icon"></i>Register
+              </a>
+              <a href="/" className="btn btn-outline-dark">
+                <i class="fa-solid fa-cart-shopping icon me-2"></i>Cart (0)
+              </a>
             </div>
           </div>
-        </nav>
-      </div>
-    );
+        </div>
+      </nav>
+    </div>
+  );
 }
