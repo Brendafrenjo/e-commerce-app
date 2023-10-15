689432af06b303d37a58665f96881db09d71b5c0
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-15 23:30:08 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,15 +7,15 @@ export default function Home() {
   return (
     <div>
       <div className="hero">
-        <div class="card text-bg-dark">
+        <div className="card text-bg-dark">
           <img
             src="./Images/fashion-background.png"
             className="card-img img-fluid"
             alt="Background"
           />
-          <div class="card-img-overlay">
-            <h1 class="card-title">Chic. Timeless. Frenjo's</h1>
-            <p class="card-text">
+          <div className="card-img-overlay">
+            <h1 className="card-title">Chic. Timeless. Frenjo's</h1>
+            <p className="card-text">
               <a href="/" className="btn btn-light primary-button">
                 SHOP NOW
               </a>
