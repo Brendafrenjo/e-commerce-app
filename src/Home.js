31f1718e75ebad917402d93fa626bf93e360ad8e
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-15 23:13:25 +0300

    Added useState on the home page

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,29 @@
-import React from "react";
-import "./Home.css"
+import React, { useState } from "react";
+import "./Home.css";
 
 export default function Home() {
+  const [state, setState] = useState({ appTitle: "Customers" });
+
   return (
-    <div className="hero">
-      <div class="card text-bg-dark">
-        <img src="./Images/fashion-background.png" className="card-img img-fluid" alt="Background" />
-        <div class="card-img-overlay">
-          <h1 class="card-title">Chic. Timeless. Frenjo's</h1>
-          <p class="card-text">
-            < a href="/" className="btn btn-light primary-button">SHOP NOW</a>
-          </p>
+    <div>
+      <div className="hero">
+        <div class="card text-bg-dark">
+          <img
+            src="./Images/fashion-background.png"
+            className="card-img img-fluid"
+            alt="Background"
+          />
+          <div class="card-img-overlay">
+            <h1 class="card-title">Chic. Timeless. Frenjo's</h1>
+            <p class="card-text">
+              <a href="/" className="btn btn-light primary-button">
+                SHOP NOW
+              </a>
+            </p>
+          </div>
         </div>
       </div>
+      <h4>{state.appTitle}</h4>
     </div>
   );
 }
