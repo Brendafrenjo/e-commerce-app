2c2c790a3f8c6a9ec2c45b1027e918e3503dd961
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-16 22:41:18 +0300

    Update Products.js

--- src/Products.js
+++ src/Products.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
 
 export default function Product() {
+  const [data, setData] = useSatte([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  let componentMounted = true;
+
+
   return <div></div>;
 }
