df256485bdd4dfdf8c0918c5f468d1b33538a0ed
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-19 23:10:02 +0300

    UpdatedProduct.js

--- src/Products.js
+++ src/Products.js
@@ -1,17 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Products.css";
+import MainContent from "./MainContent"
 
 export default function Products() {
-  const [data, setData] = useState(5);
-
-  const state = { appTitle: "Customers", customersCount: data };
-
-  function handleRefresh(event) {
-    console.log("Refresh clicked");
-    event.preventDefault();
-    setData(10);
-  }
-
   return (
     <div className="Products">
       <div className="row">
@@ -20,13 +11,7 @@ export default function Products() {
           <hr />
         </div>
       </div>
-      <h4>
-        {state.appTitle}{" "}
-        <span className="badge bg-success">{state.customersCount}</span>
-        <button className="btn btn-success ms-2" onClick={handleRefresh}>
-          Refresh
-        </button>
-      </h4>
+      <MainContent />
     </div>
   );
 }
