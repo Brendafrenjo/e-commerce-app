d4b67611144b488be58138a6eda3d6f98c8e7e09
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-19 22:00:35 +0300

    Created Products.css

--- src/Products.js
+++ src/Products.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./Products.css";
 
 export default function Products() {
-  return (<div>
-    <h1>Customers</h1>
-  </div>);
+  return (
+    <div className="Products">
+      <h1>Customers</h1>
+    </div>
+  );
 }
