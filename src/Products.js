d9b0af387b9f21e934d862f8d9f39515bd58bbce
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-20 23:27:38 +0300

    Added Loader

--- src/Products.js
+++ src/Products.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Products.css";
+import { FallingLines } from "react-loader-spinner";
 import MainContent from "./MainContent";
 
 export default function Products() {
-  return (
-    <div className="Products">
-      <div className="row">
-        <div className="col-12">
-          <h1>Latest Products</h1>
-          <hr />
-          <div className="buttons d-flex justify-content-center mb-5 pb-5">
-            <button className="btn btn-outline-dark me-2">Cargo Pants</button>
-            <button className="btn btn-outline-dark me-2">Dresses</button>
-            <button className="btn btn-outline-dark me-2">Shoes</button>
-            <button className="btn btn-outline-dark me-2">
-              Shirts and Tops
-            </button>
-            <button className="btn btn-outline-dark">Accessories</button>
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  if (loading) {
+    return (
+      <div className="Products">
+        <div className="row">
+          <div className="col-12">
+            <h1>Latest Products</h1>
+            <hr />
+            <div className="buttons d-flex justify-content-center mb-5 pb-5">
+              <button className="btn btn-outline-dark me-2">Cargo Pants</button>
+              <button className="btn btn-outline-dark me-2">Dresses</button>
+              <button className="btn btn-outline-dark me-2">Shoes</button>
+              <button className="btn btn-outline-dark me-2">
+                Shirts and Tops
+              </button>
+              <button className="btn btn-outline-dark">Accessories</button>
+            </div>
           </div>
         </div>
+        <MainContent />
       </div>
-      <MainContent />
-    </div>
-  );
+    );
+  } else {
+    setLoading();
+    return (
+      <div>
+      <FallingLines
+        color="#4fa94d"
+        width="100"
+        visible={true}
+        ariaLabel="falling-lines-loading"
+        />
+        </div>
+    );
+  }
 }
