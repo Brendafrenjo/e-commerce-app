cb0cdb171e4a6ce3fc9ac71793c6a10a42ccebb4
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-25 20:55:20 +0300

    Added useEffect

--- src/Products.js
+++ src/Products.js
@@ -4,12 +4,29 @@ import { FallingLines } from "react-loader-spinner";
 import MainContent from "./MainContent";
 
 const Products = () => {
-
   export default function Products() {
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
     const [filter, setFilter] = useState(initialState);
     let componentMounted = true;
+
+    useEffect(() => {
+      const getProducts = async () => {
+        setLoading(true);
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (componentMounted) {
+          setData(await response.clone().json());
+          setFilter(await response.json());
+          setLoading(false);
+        }
+      };
+
+      getProducts();
+
+      return () => {
+        componentMounted = false;
+      };
+    }, []);
 
     if (loading) {
       return (
@@ -19,7 +36,9 @@ const Products = () => {
               <h1>Latest Products</h1>
               <hr />
               <div className="buttons d-flex justify-content-center mb-5 pb-5">
-                <button className="btn btn-outline-dark me-2">Cargo Pants</button>
+                <button className="btn btn-outline-dark me-2">
+                  Cargo Pants
+                </button>
                 <button className="btn btn-outline-dark me-2">Dresses</button>
                 <button className="btn btn-outline-dark me-2">Shoes</button>
                 <button className="btn btn-outline-dark me-2">
@@ -45,4 +64,4 @@ const Products = () => {
       );
     }
   }
-}
+};
