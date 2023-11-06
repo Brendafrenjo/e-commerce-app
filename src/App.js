01ae129e4d59a3be51e7c2010e017824ac107582
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-06 23:46:42 +0300

    Removed router-dom

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import React from "react";
 import "./App.css";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import { Switch, Route } from "react-router-dom";
-import Products from "./MainContent";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Switch>
-        <Home />
-        <Router exact path="/" component={Home} />
-        <Router exact path="/Products" component={Products} />
-      </Switch>
+      <Home />
     </div>
   );
 }
