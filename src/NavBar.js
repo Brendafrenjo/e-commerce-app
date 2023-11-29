4c522d37203e34bf2facba59abd2b5042a59ea01
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-29 05:40:50 +0300

    Updated NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -25,7 +25,7 @@ export default function NavBar() {
     <div className="NavBar">
       <nav className="navbar navbar-expand-lg bg-white navbar-style">
         <div className="container-fluid">
-          <NavLink className="navbar-brand fw-bold fs-4" href="/">
+          <NavLink className="navbar-brand fw-bold fs-4" to="/">
             <Images />
           </NavLink>
           <button
@@ -42,35 +42,35 @@ export default function NavBar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link active" href="/">
+                <NavLink className="nav-link active" to="/">
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <a className="nav-link" to="/">
                   Products
                 </a>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" href="/">
+                <NavLink className="nav-link" to="/">
                   About
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" href="/">
+                <NavLink className="nav-link" to="/">
                   Contact
                 </NavLink>
               </li>
             </ul>
             <div className="buttons">
-              <NavLink href="/" className="btn btn-outline-dark me-2">
+              <NavLink to="/" className="btn btn-outline-dark me-2">
                 <i className="fa-solid fa-right-to-bracket me-2 icon"></i>Login
               </NavLink>
-              <NavLink href="/" className="btn btn-outline-dark me-2">
-                <i class="fa-solid fa-user-plus me-2 icon"></i>Register
+              <NavLink to="/" className="btn btn-outline-dark me-2">
+                <i className="fa-solid fa-user-plus me-2 icon"></i>Register
               </NavLink>
-              <NavLink href="/" className="btn btn-outline-dark">
-                <i class="fa-solid fa-cart-shopping icon me-2"></i>Cart (0)
+              <NavLink to="/" className="btn btn-outline-dark">
+                <i className="fa-solid fa-cart-shopping icon me-2"></i>Cart (0)
               </NavLink>
             </div>
           </div>
