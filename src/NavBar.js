6cf0af812fa13336b6f823015e95981917cc94ee
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-29 05:28:11 +0300

    Imported NavLink

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./NavBar.css";
 import Images from "./Images";
+import { NavLink } from "react-router-dom"
 
 export default function NavBar() {
   function handleResize() {
@@ -24,9 +25,9 @@ export default function NavBar() {
     <div className="NavBar">
       <nav className="navbar navbar-expand-lg bg-white navbar-style">
         <div className="container-fluid">
-          <a className="navbar-brand fw-bold fs-4" href="/">
+          <NavLink className="navbar-brand fw-bold fs-4" href="/">
             <Images />
-          </a>
+          </NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -41,9 +42,9 @@ export default function NavBar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" href="/">
+                <NavLink className="nav-link active" href="/">
                   Home
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/">
@@ -51,26 +52,26 @@ export default function NavBar() {
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <NavLink className="nav-link" href="/">
                   About
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <NavLink className="nav-link" href="/">
                   Contact
-                </a>
+                </NavLink>
               </li>
             </ul>
             <div className="buttons">
-              <a href="/" className="btn btn-outline-dark me-2">
+              <NavLink href="/" className="btn btn-outline-dark me-2">
                 <i className="fa-solid fa-right-to-bracket me-2 icon"></i>Login
-              </a>
-              <a href="/" className="btn btn-outline-dark me-2">
+              </NavLink>
+              <NavLink href="/" className="btn btn-outline-dark me-2">
                 <i class="fa-solid fa-user-plus me-2 icon"></i>Register
-              </a>
-              <a href="/" className="btn btn-outline-dark">
+              </NavLink>
+              <NavLink href="/" className="btn btn-outline-dark">
                 <i class="fa-solid fa-cart-shopping icon me-2"></i>Cart (0)
-              </a>
+              </NavLink>
             </div>
           </div>
         </div>
