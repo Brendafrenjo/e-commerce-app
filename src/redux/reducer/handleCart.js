8131114e829c9369616acf27023cfea069631eda
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-17 04:49:45 +0300

    Update handleCart.js

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -4,7 +4,35 @@ function handleCart(state = cart, action) {
   const product = action.payload;
   switch (action.type) {
     case "ADD_ITEM":
-     
+      //Check if Product is Already Exist//
+      const exist = find((x) => x.id === product.id);
+      if (exist) {
+        //Increase the Quantity//
+        return state.map(() =>
+          x.id === product.id ? { ...x, qty: x.qty + 1 } : x
+        );
+      } else {
+        const product = action.payload;
+        return [
+          ...state,
+          {
+            ...product,
+            qty: 1,
+          },
+        ];
+      }
+    case "DELETE_ITEM":
+      const exist1 = state.find((x) => x.id === product.id);
+      if (exist1.qty === 1) {
+        return state.filter((x) => x.id !== exist1.id);
+      } else {
+        return state.map((x) =>
+          x.id === product.id ? { ...x, qty: x.qty - 1 } : x
+        );
+      }
+      break;
+    default:
+      break;
   }
 }
 
