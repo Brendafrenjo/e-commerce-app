7be0f8edb71eff16b3efb9e8b16c2271c88fefd2
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-17 04:47:47 +0300

    Update handleCart.js

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -4,16 +4,7 @@ function handleCart(state = cart, action) {
   const product = action.payload;
   switch (action.type) {
     case "ADD_ITEM":
-      return {
-        ...state,
-        items: [...state.items, action.payload],
-      };
-    case "DELETE_ITEM":
-      return {
-        items: [...state.items.filter((item) => item !== action.payload)],
-      };
-    default:
-      return state;
+     
   }
 }
 
