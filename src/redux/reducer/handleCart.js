9f8876c0c557812b6bee01e1891eeec75bddfb80
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-20 11:57:17 +0300

    Update handleCart.js

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -1,37 +1,49 @@
-const cart = [];
+const initialState = {
+  cart: [],
+};
 
-function handleCart(state = cart, action) {
+function handleCart(state = initialState, action) {
   const product = action.payload;
+
   switch (action.type) {
     case "ADD_ITEM":
-      //Check if Product is Already Exist//
-      const exist = state.find((x) => x.id === product.id);
+      const exist = state.cart.find((x) => x.id === product.id);
       if (exist) {
-        //Increase the Quantity//
-        return state.map((x) =>
-          x.id === product.id ? { ...x, qty: x.qty + 1 } : x
-        );
-      } else {
-        const product = action.payload;
-        return [
+        return {
           ...state,
-          {
-            ...product,
-            qty: 1,
-          },
-        ];
+          cart: state.cart.map((x) =>
+            x.id === product.id ? { ...x, qty: x.qty + 1 } : x
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cart: [
+            ...state.cart,
+            {
+              ...product,
+              qty: 1,
+            },
+          ],
+        };
       }
 
     case "DELETE_ITEM":
-      const exist1 = state.find((x) => x.id === product.id);
+      const exist1 = state.cart.find((x) => x.id === product.id);
       if (exist1.qty === 1) {
-        return state.filter((x) => x.id !== exist1.id);
+        return {
+          ...state,
+          cart: state.cart.filter((x) => x.id !== exist1.id),
+        };
       } else {
-        return state.map((x) =>
-          x.id === product.id ? { ...x, qty: x.qty - 1 } : x
-        );
+        return {
+          ...state,
+          cart: state.cart.map((x) =>
+            x.id === product.id ? { ...x, qty: x.qty - 1 } : x
+          ),
+        };
       }
-      
+
     default:
       return state;
   }
