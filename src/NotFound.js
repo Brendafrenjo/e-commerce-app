7e502b695e854336b23f6013d338d2b86d94f4ad
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-20 15:47:19 +0300

    Updated NotFound.js

--- src/NotFound.js
+++ src/NotFound.js
@@ -3,7 +3,7 @@ import "./NotFound.css"
 
 export default function NotFound() {
   return (
-    <div className="">
+    <div className="NotFound">
       <h2>404</h2>
       <p>Page not found</p>
     </div>
