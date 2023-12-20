2ec2dfdabe7343a745433cee6cd3465651dc1104
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-20 14:50:32 +0300

    Added the NotFound component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import "./App.css";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Products from "./Products";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Redirect } from "react-router-dom";
 import Product from "./Product";
 import LogIn from "./LogIn";
+import Cart from "./Cart";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="products/:id" element={<Product />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/not-found" element={<NotFound />} />
+        <Route exact path="/" element={<Home />} />
       </Routes>
     </div>
   );
---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NotFound.css"
+
+export default function NotFound() {
+  return (
+    <div className="">
+      <h2>404</h2>
+      <p>Page not found</p>
+    </div>
+  );
+}
