3c70c6115b6a2d499a43550cda04f1672ed94e87
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-25 22:38:41 +0300

    Update handleCart.js

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -6,24 +6,31 @@ function handleCart(state = cart, action) {
   const product = action.payload;
   switch (action.type) {
     case "ADD_ITEM":
-      //Check if Product is Already Exist//
+      // Check if the product is already in the cart//
       const existingProduct = state.find((x) => x.id === product.id);
       if (existingProduct) {
-        //Increase the Quantity//
+        // If the product is in the cart, increase its quantity//
         toast.info(`${existingProduct.title} cart quantity increased`, {
           position: "bottom-left",
         });
+        // Update the state by mapping through the existing items
+        // and incrementing the quantity of the matching product
         const updatedState = state.map((x) =>
           x.id === product.id ? { ...x, qty: x.qty + 1 } : x
         );
 
+        // Update the local storage with the updated state
         localStorage.setItem("cartItems", JSON.stringify(updatedState));
+        // Return the updated state
         return updatedState;
       } else {
+        // If the product is not in the cart, add it with quantity 1
         toast.success(`${action.payload.title} added to cart`, {
           position: "bottom-left",
         });
 
+        // Create a new state array by spreading the existing items
+        // and adding a new item with quantity 1
         const updatedState = [
           ...state,
           {
@@ -31,7 +38,11 @@ function handleCart(state = cart, action) {
             qty: 1,
           },
         ];
+
+        // Update the local storage with the updated state
         localStorage.setItem("cartItems", JSON.stringify(updatedState));
+
+        // Return the updated state
         return updatedState;
       }
 
