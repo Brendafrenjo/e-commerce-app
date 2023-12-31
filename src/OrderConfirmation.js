09f193589c5032129c7f47cb938c0a2c8fc2b38b
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-01 01:14:18 +0300

    Update OrderConfirmation.js

--- src/OrderConfirmation.js
+++ src/OrderConfirmation.js
@@ -1,96 +1,87 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import "./OrderConfirmation.css";
-import { initializeOrderDetails } from "./redux/action";
+import React, { useState} from "react";
 
-export default function OrderConfirmation({ selectedPaymentMethod }) {
-  const { handleCart, handleOrderDetails } = useSelector((state) => state);
-  const dispatch = useDispatch();
+export default function OrderConfirmation() {
+  const [paymentMethod, setPaymentMethod] = useState("");
 
-  useEffect(() => {
-    // Simulate initializing order details using the existing cart details
-    const simulatedOrderDetails = {
-      orderNumber: generateOrderNumber(),
-      items: handleCart,
-      totalAmount: calculateSubtotal(handleCart),
-      shippingInfo: {
-        firstName: "John",
-        lastName: "Doe",
-        email: "john.doe@example.com",
-        tel: "123-456-7890",
-        address: "123 Main St, Cityville",
-      },
-      orderDate: new Date(),
-      paymentMethod: selectedPaymentMethod,
-    };
 
-    dispatch(initializeOrderDetails(simulatedOrderDetails));
-  }, [dispatch, handleCart, selectedPaymentMethod]);
 
-  const calculateSubtotal = (cart) => {
-    return cart.reduce((total, product) => {
-      return total + product.qty * product.price;
-    }, 0);
-  };
-
-  const generateOrderNumber = () => {
-    return Math.floor(Math.random() * 1000000) + 1;
+  const handlePaymentChange = (event) => {
+    setPaymentMethod(event.target.value);
   };
 
   const handlePayNow = () => {
-    window.location.href = "https://mpesa-payment-url.com";
+    if (paymentMethod) {
+      alert(
+        `Payment successful! Thank you for your purchase!`
+      );
+      // You can also redirect the user to a confirmation page or perform other actions.
+    } else {
+      alert("Please select a payment method before proceeding.");
+    }
   };
-
-  if (!handleOrderDetails) {
-    // Handle the case where order details are not available yet
-    return <p>Loading order details...</p>;
-  }
 
   return (
     <div className="OrderConfirmation">
-      <div className="OrderConfirmation">
-        <h2>Order Confirmation</h2>
-        <p>Your order has been successfully placed!</p>
-        <div className="order-details">
-          <h3>Order Details</h3>
-          <div>
-            <strong>Order Number:</strong>
-          </div>
-          <div>
-            <strong>Items:</strong>
-            <div>
-              {handleOrderDetails.items.map((item, index) => (
-                <div key={index}>
-                  {item.name} - Quantity: {item.quantity}
-                </div>
-              ))}
-            </div>
-          </div>
-          <div>
-            <strong>Total Amount:</strong> $
-            {handleOrderDetails.totalAmount.toFixed(2)}
-          </div>
-          <div>
-            <strong>Shipping Information:</strong>
-            <div>
-              <div>
-                Name: {handleOrderDetails.shippingInfo.firstName}{" "}
-                {handleOrderDetails.shippingInfo.lastName}
-              </div>
-              <div>Email: {handleOrderDetails.shippingInfo.email}</div>
-              <div>Phone: {handleOrderDetails.shippingInfo.tel}</div>
-              <div>
-                Address: {handleOrderDetails.shippingInfo.town},{" "}
-                {handleOrderDetails.shippingInfo.county},{" "}
-                {handleOrderDetails.shippingInfo.zipCode}
-              </div>
-            </div>
-          </div>
-          <div>
-            <button onClick={handlePayNow}>Pay Now</button>
-          </div>
-        </div>
+      <h2>Order Confirmation</h2>
+      <p>Your order has been successfully placed!</p>
+      <h2>Order Summary</h2>
+      <p>
+        <strong>Product:</strong> Laptop
+      </p>
+      <p>
+        <strong>Quantity:</strong> 1
+      </p>
+      <p>
+        <strong>Total Amount:</strong> $1000.00
+      </p>
+
+      <h3>Client Information</h3>
+      <p>
+        <strong>First Name:</strong> 
+      </p>
+      <p>
+        <strong>Last Name:</strong>
+      </p>
+      <p>
+        <strong>Email:</strong> 
+      </p>
+      <p>
+        <strong>Phone Number:</strong> 
+      </p>
+      <p>
+        <strong>Location:</strong> 
+      </p>
+      <div>
+        <h3>Payment Methods</h3>
+        <label>
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="creditCard"
+            onChange={handlePaymentChange}
+          />
+          Credit Card
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="paypal"
+            onChange={handlePaymentChange}
+          />
+          PayPal
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="paymentMethod"
+            value="bankTransfer"
+            onChange={handlePaymentChange}
+          />
+          Bank Transfer
+        </label>
       </div>
+      <button onClick={handlePayNow}>Pay Now</button>
     </div>
   );
 }
