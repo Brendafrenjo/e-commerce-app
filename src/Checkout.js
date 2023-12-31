ccd29449d4446dd9ff80968a8ff26127334e3954
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-12-31 14:19:22 +0300

    Created Checkout.css

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import "./Checkout.css";
 
 export default function Checkout() {
   const [orderPlaced, setOrderPlaced] = useState(false);
@@ -62,82 +63,84 @@ export default function Checkout() {
     setOrderPlaced(true);
   };
   return (
-    <div>
-      {orderPlaced && <p>Redirecting...</p>}
-      <h1>Checkout</h1>
-      <p>Please fill your delivery information</p>
-      <form>
-        <div className="row">
-          <div className="col-4">
-            <div className="form-group mb-2">
-              <select className="form-select form-control">
-                <option value="254">
-                  <span className="flag-icon flag-icon-ke">🇰🇪</span>+254
-                </option>
-                <option value="234">
-                  <span className="flag-icon flag-icon-ng">🇹🇿</span>+255
-                </option>
-                <option value="256">
-                  <span className="flag-icon flag-icon-ug">🇺🇬</span>+256
-                </option>
-              </select>
+    <div className="Checkout">
+      <div className="contained">
+        {orderPlaced && <p>Redirecting...</p>}
+        <h1>Checkout</h1>
+        <p>Please fill your delivery information</p>
+        <form>
+          <div className="row">
+            <div className="col-4">
+              <div className="form-group mb-2">
+                <select className="form-select form-control">
+                  <option value="254">
+                    <span className="flag-icon flag-icon-ke">🇰🇪</span>+254
+                  </option>
+                  <option value="234">
+                    <span className="flag-icon flag-icon-ng">🇹🇿</span>+255
+                  </option>
+                  <option value="256">
+                    <span className="flag-icon flag-icon-ug">🇺🇬</span>+256
+                  </option>
+                </select>
+              </div>
+            </div>
+            <div className="col-8">
+              <div className="form-group">
+                <input
+                  type="tel"
+                  placeholder="e.g. 700200100"
+                  className="form-control"
+                />
+              </div>
             </div>
           </div>
-          <div className="col-8">
-            <div className="form-group">
-              <input
-                type="tel"
-                placeholder="e.g. 700200100"
-                className="form-control"
-              />
-            </div>
-          </div>
-        </div>
-      </form>
-      <form>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          placeholder="First name"
-          className="form-control mb-2"
-        />
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          placeholder="Last name"
-          className="form-control mb-2"
-        />
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="name@example.com"
-          className="form-control mb-2"
-        />
-        <input
-          type="text"
-          id="zipCode"
-          name="zipCode"
-          placeholder="e.g. 00100"
-          className="form-control mb-2"
-        />
-        <select className="form-select form-control mb-2">
-          <option value="" selected>
-            Select a County
-          </option>
-          {counties.map((county, index) => (
-            <option key={index} value={index + 1}>
-              {county}
+        </form>
+        <form>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            placeholder="First name"
+            className="form-control mb-2"
+          />
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            placeholder="Last name"
+            className="form-control mb-2"
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="name@example.com"
+            className="form-control mb-2"
+          />
+          <input
+            type="text"
+            id="zipCode"
+            name="zipCode"
+            placeholder="e.g. 00100"
+            className="form-control mb-2"
+          />
+          <select className="form-select form-control mb-2">
+            <option value="" selected>
+              Select a County
             </option>
-          ))}
-        </select>
-        <input type="text" placeholder="Town" className="form-control mb-2" />
-      </form>
-      <button type="submit" className="btn btn-dark mt-2">
-        Place Order
-      </button>
+            {counties.map((county, index) => (
+              <option key={index} value={index + 1}>
+                {county}
+              </option>
+            ))}
+          </select>
+          <input type="text" placeholder="Town" className="form-control mb-2" />
+        </form>
+        <button type="submit" className="btn btn-dark mt-2 place-order">
+          Place Order
+        </button>
+      </div>
     </div>
   );
 }
