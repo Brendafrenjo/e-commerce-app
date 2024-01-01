20907a32dd90e8f76e3175888e191bbcb8274e94
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-02 02:15:03 +0300

    Update LogIn.js

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function LogIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = () => {
+    // Add any validation logic here
+    onSignIn({ username, password });
+  };
+
   return (
     <div>
       <div className="row">
         <div className="col-md-9">
           <h1>LogIn</h1>
+          <label htmlFor="signInUsername">Username:</label>
+          <input
+            type="text"
+            id="signInUsername"
+            name="signInUsername"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+
+          <label htmlFor="signInPassword">Password:</label>
+          <input
+            type="password"
+            id="signInPassword"
+            name="signInPassword"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="button" onClick={handleSignIn}>
+            LogIn
+          </button>
         </div>
       </div>
     </div>
