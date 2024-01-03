44454f9fa7248b1888c0588b4c18dd98ecb10a35
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-03 04:37:12 +0300

    Created FindYourAccount.css

--- src/FindYourAccount.js
+++ src/FindYourAccount.js
@@ -1,24 +1,29 @@
 import React from "react";
+import "./FindYourAccount.css";
 
 export default function FindYourAccount() {
   return (
     <div className="FindYourAccount">
-      <h3>Find Your Account</h3>
+      <h3 className="text-start">Find Your Account</h3>
       <hr />
-      <p>Please enter your email address to search for your account.</p>
+      <p className="text-start">
+        Please enter your email address to search for your account.
+      </p>
       <hr />
       <form>
         <input
           type="email"
           id="email"
           name="email"
-          placeholder="Email adress"
+          placeholder="Email address"
           className="form-control log-in-form"
         />
       </form>
       <hr />
-      <button className="btn btn-outline-dark ms-2">Cancel</button>
-      <button className="btn btn-dark">Search</button>
+      <div className="buttons">
+        <button className="btn btn-outline-dark me-2 cancel-button">Cancel</button>
+        <button className="btn btn-dark search-button">Search</button>
+      </div>
     </div>
   );
 }
