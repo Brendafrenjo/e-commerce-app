8d79907e1d7de760ba432d74332d6ef67a7be2ca
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-03 03:14:33 +0300

    Hide the navbar for the authentification pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Products from "./Products";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Product from "./Product";
 import LogIn from "./LogIn";
 import Cart from "./Cart";
@@ -13,12 +13,19 @@ import NotFound from "./NotFound";
 import Checkout from "./Checkout";
 import OrderConfirmation from "./OrderConfirmation";
 import AuthenticationContainer from "./AuthenticationContainer";
+import Register from "./Register";
 
 function App() {
+const location = useLocation();
+const hideNavBar =
+  location.pathname === "/authentication-container" ||
+  location.pathname === "/register" ||
+  location.pathname === "/login";
+
   return (
     <div className="App">
       <ToastContainer />
-      <NavBar />
+      {!hideNavBar && <NavBar />}
       <Routes>
         <Route path="/products" element={<Products />} />
         <Route path="products/:id" element={<Product />} />
@@ -31,6 +38,7 @@ function App() {
           path="/authentication-container"
           element={<AuthenticationContainer />}
         />
+        <Route path="/register" element={<Register />} />
         <Route exact path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/not-found" />} />
       </Routes>
