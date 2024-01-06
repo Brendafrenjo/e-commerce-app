82d227a4e90410c9d099cc9aca3e3685102f7234
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-06 23:25:24 +0300

    Updated AuthenticationContainer

--- src/AuthenticationContainer.js
+++ src/AuthenticationContainer.js
@@ -6,14 +6,32 @@ import "./AuthenticationContainer.css";
 export default function AuthenticationContainer() {
   const [isSignIn, setIsSignIn] = useState(true);
 
+  function toggleSignIn() {
+    setIsSignIn(!isSignIn);
+  }
+
   return (
     <div className="AuthenticationContainer">
       {isSignIn ? <LogIn /> : <Registration />}
 
-      <p onClick={() => setIsSignIn(!isSignIn)} className="mt-3 isSignIn">
-        {isSignIn
-          ? "Don't have an account? Register here."
-          : "Already have an account? Log in here."}
+      <p className="mt-3">
+        {isSignIn ? (
+          <>
+            Don't have an account?{" "}
+            <span onClick={toggleSignIn} className="isSignIn">
+              {" "}
+              Register here.
+            </span>
+          </>
+        ) : (
+          <>
+            Already have an account?{" "}
+            <span onClick={toggleSignIn} className="isSignIn">
+              {" "}
+              Log in here.
+            </span>
+          </>
+        )}
       </p>
     </div>
   );
