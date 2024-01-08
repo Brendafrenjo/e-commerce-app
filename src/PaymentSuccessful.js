69f47db6f2cb54486a00473bc1f44dc73ac6dd38
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-08 23:02:06 +0300

    Create PaymentSuccessful.js

---
+++ src/PaymentSuccessful.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PaymentSuccesful() {
+    return (
+      <div className="PaymentSuccesful">
+        <h1>Payment successful!</h1>
+        <h1>Payment successful! Thank you for your purchase</h1>
+      </div>
+    );
+}
