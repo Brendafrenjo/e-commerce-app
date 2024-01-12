f5073cf632cf66cac805370b88a9012f78250d32
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-13 02:27:19 +0300

    Update Footer

--- src/Footer.js
+++ src/Footer.js
@@ -11,10 +11,36 @@ export default function Footer() {
 
   return (
     <div className="Footer">
-      <small className="footer">Frenjo Fashions</small>
-      <button className="btn btn scroll-to-top-button" onClick={scrollToTop}>
-        <i className="fa-solid fa-chevron-up icon"></i>
-      </button>
+      <div className="footer-information">
+        <span>
+          <h1 className="find-us">Find Us</h1>
+          <div className="find-us-information">
+            <h3 className="find-us-heading">Physical Location:</h3>
+            <p className="find-us-content">
+              Altman Street, 2nd Floor, Shop B2, Nairobi Kenya
+              <br />
+              House of Landing, 4th Floor, Lavington, Nairobi
+            </p>
+            <h3 className="find-us-heading">Opening Hours:</h3>
+            <p>
+              Mon - start
+              <br />
+              8am - 8pm
+            </p>
+            <h3 className="find-us-heading">Phone Number:</h3>
+            <p>(+254)001002003</p>
+          </div>
+        </span>
+        <span>
+          <span className="footer">Frenjo Fashions</span>
+          <button
+            className="btn btn scroll-to-top-button"
+            onClick={scrollToTop}
+          >
+            <i className="fa-solid fa-chevron-up icon"></i>
+          </button>
+        </span>
+      </div>
     </div>
   );
 }
