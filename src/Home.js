8fb4378879b17c502eb19142fcfc40838af23366
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-01-13 10:15:22 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Products from "./Products";
 import "./Home.css";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -15,13 +16,13 @@ export default function Home() {
           <div className="card-img-overlay">
             <h1 className="card-title">Chic. Timeless. Frenjo's</h1>
             <p className="card-text">
-              <a
+              <NavLink
                 href="/products"
                 className="btn btn-light primary-button"
                 title="Shop Now"
               >
                 SHOP NOW
-              </a>
+              </NavLink>
             </p>
           </div>
         </div>
